c895faf21ca292f8d56ae135c01895e452849b92
daniel-toplian <danieltoplian@gmail.com>
2024-09-07 23:13:20 +0300

    Add controller for handling Rest mockApi requests

---
+++ src/main/java/copyCat/controllers/ApiController.java
@@ -0,0 +1,31 @@
+package copyCat.controllers;
+
+import copyCat.entities.ApiMock;
+import copyCat.services.ApiService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class ApiController {
+    private final ApiService apiService;
+
+    @Autowired
+    public ApiController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @RequestMapping("/**")
+    public ResponseEntity<String> replayToRestRequest(HttpServletRequest request) {
+        String requestedUrl = request.getRequestURI();
+        Optional<ApiMock> api = apiService.getByUrl(requestedUrl);
+
+        return api.map(apiMock -> ResponseEntity.status(apiMock.statusCode()).body(apiMock.response()))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("API Mock not found for URL: " + requestedUrl));
+    }
+}
