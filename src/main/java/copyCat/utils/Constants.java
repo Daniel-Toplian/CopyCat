3957d219ceee23ae1b23d5911799f9ae72ceb3c9
daniel-toplian <danieltoplian@gmail.com>
2024-09-22 22:47:28 +0300

    Add test for MockRequestController

--- src/main/java/copyCat/utils/Constants.java
+++ src/main/java/copyCat/utils/Constants.java
@@ -8,4 +8,7 @@ public class Constants {
     // --- config ---
     public static final String CONFIG_PATH = "CONFIG_PATH";
     public static final String USING_FILE_RECOVERY = "usingFileRecovery";
+
+    // --- messages ---
+    public static final String API_NOT_FOUND = "API Mock not found for URL: %s";
 }
---
+++ src/test/controllers/MockRequestControllerTest.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import copyCat.controllers.MockRequestController;
+import copyCat.entities.ApiMock;
+import copyCat.entities.RestMock;
+import copyCat.entities.Role;
+import copyCat.services.ApiService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static copyCat.utils.Constants.API_NOT_FOUND;
+
+@ContextConfiguration(classes = MockRequestController.class)
+@WebMvcTest(MockRequestController.class)
+public class MockRequestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ApiService apiService;
+
+    private final String TEST_URL = "/test";
+
+    @Test
+    public void replayToRestRequest_found() throws Exception {
+        ApiMock apiMock = new RestMock.Builder()
+                .id(UUID.randomUUID())
+                .name("MockRequestController-Tests")
+                .url(TEST_URL)
+                .response("response")
+                .role(Role.SERVER.toString())
+                .httpMethod(HttpMethod.GET.name())
+                .statusCode(200)
+                .build();
+
+        Mockito.when(apiService.getByUrl(TEST_URL)).thenReturn(Optional.of(apiMock));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(TEST_URL).accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(apiMock.statusCode()))
+                .andExpect(MockMvcResultMatchers.content().string(apiMock.response()));
+    }
+
+    @Test
+    public void replayToNonExistingRestRequest_notFound() throws Exception {
+        Mockito.when(apiService.getByUrl(TEST_URL)).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders.get(TEST_URL).accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().string(API_NOT_FOUND.formatted(TEST_URL)));
+    }
+}
