e32c8653471c89bc62b56fd6eb9426ff4948cbe8
daniel-toplian <danieltoplian@gmail.com>
2024-10-05 20:51:02 +0300

    Add properties default values and print them after startup

--- src/main/java/copyCat/Application.java
+++ src/main/java/copyCat/Application.java
@@ -2,19 +2,40 @@ package copyCat;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private ConfigurableEnvironment env;
     private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
-        String port = run(Application.class, args).getEnvironment().getProperty("server.port");
+        Application app = run(Application.class, args).getBean(Application.class);
+        app.startupMessage();
+    }
+
+    private void startupMessage() {
+        StringBuilder propertiesLog = new StringBuilder();
+        propertiesLog.append("\nStarting CopyCat application with properties:\n");
+
+        env.getPropertySources().stream().filter(source -> source.getName().contains("application.properties")).findFirst().ifPresent(source -> {
+            if (source.getSource() instanceof java.util.Map) {
+                for (Object key : ((java.util.Map<?, ?>) source.getSource()).keySet()) {
+                    propertiesLog.append(key).append("=")
+                            .append(env.getProperty(key.toString())).append("\n");
+                }
+            }
+        });
+
+        LOGGER.info(propertiesLog.toString());
         LOGGER.info("#######################################################");
-        LOGGER.info("      CopyCat is up and running on port: {}", port);
+        LOGGER.info("      CopyCat is up and running on port: {}", env.getProperty("server.port"));
         LOGGER.info("#######################################################");
     }
 }
